af356b257cac68f0f6267857c23ce54dcfc76750
duyhn1 <33491545+duyhn1@users.noreply.github.com>
2017-11-12 23:24:43 +0900

    Add files via upload

---
+++ SkypeCommunicate/SkypeCommunicate/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace SkypeCommunicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
